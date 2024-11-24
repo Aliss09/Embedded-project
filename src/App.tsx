e8b575ef5220fb8cfc6d20d1cc8eaf2f77934488
Tuckkrub00 <145004375+Tuckkrub00@users.noreply.github.com>
2024-11-24 09:04:27 +0700

    setup ui

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <img src=""></img>
+      <h1 className="text-4xl font-bold text-blue-600">Hello, TailwindCSS!</h1>
+    </div>
+  );
+}
+
+export default App;
